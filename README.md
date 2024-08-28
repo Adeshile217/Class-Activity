cbb192721a5320c3cdce0cc8e2b1ecacb3ffd813
Adeshile Ibraheem <sadeshile@gmail.com>
2024-08-28 06:19:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Activities
+
+- Create a Github repo for your portfolio and push your codebase into the Github repository
+
+- Create a pen for your portfolio and add your portfolio markup (HTML), CSS and JavaScript (if any)
+
+- Create a program that stores a student score in a variable, and use conditional logic to print the grade dependending on the value of the student score
---
+++ scripe.js
@@ -0,0 +1,14 @@
+
+let studentScore = 100;
+
+if ((studentScore > 90) (studentScore <= 100)) {
+  console.log("Student grade is A");
+} else if ((studentScore > 80) && (studentScore < 90)) {
+  console.log("Student grade is B");
+} else if ((studentScore > 70) && (studentScore <= 80)) {
+  console.log("Student grade is C");
+} else if ((studentScore > 50) && (studentScore <= 70)) {
+  console.log("Student grade is D");
+} else {
+  console.log("Student grade is F");
+}
